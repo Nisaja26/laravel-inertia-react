ab65697463bbd81fe95cee31ea4a61e7d1fafd14
Nisaja26 <nissaikaracmawati@gmail.com>
2024-11-15 11:26:09 +0700

    05 full

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,13 @@
-function Home({ name }) {
+import { Link } from "@inertiajs/react";
+
+export default function Home({ name }) {
     return (
       <>
-        <h1 className="title">Hallo {name}</h1>
+        <h1 className="title">Hallo {name} </h1>
+
+        <Link preserveScroll href="/" className="block title mt-[1000px]">
+        {new Date().toLocaleDateString()}</Link>
       </>
     );
 }
+ 
