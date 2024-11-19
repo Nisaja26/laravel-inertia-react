b1af577f38edef66c08b07a820df8b6544701a29
Nisaja26 <nissaikaracmawati@gmail.com>
2024-11-19 14:14:27 +0700

    06 full

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    sleep(2); 
-    return Inertia::render('Home', ['name'=> 'Nissa']);
-});
+Route::get('/', [PostController::class, 'index']);
 
+Route::resource('post', PostController::class)->except('index');
